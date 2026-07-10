34ab9c0eefe6ad73066ff1c9111bacb1babf7a75
Abhay RAjpoot <thegamma551@gmail.com>
2026-07-11 04:52:02 +0530

    Fix nodemon entry point, add dotenv config, update db connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require("./src/app");
+require('dotenv').config();
 
+const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
 connectDB()
--- src/app.js
+++ src/app.js
@@ -4,43 +4,57 @@ const noteModel = require("./models/note.model")
 const app = express();
 app.use(express.json());
 
-const notes = [];
+// const notes = [];
 
-app.post("/notes",(req,res)=>{
-    notes.push(req.body)
+// app.post("/notes",(req,res)=>{
+//     notes.push(req.body)
 
-    res.status(201).json({message:"notes created successfully"})
-});
+//     res.status(201).json({message:"notes created successfully"})
+// });
 
-app.get("/notes",(req,res) =>{
+// app.get("/notes",(req,res) =>{
 
-    res.status(200).json({
-        notes:notes
+//     res.status(200).json({
+//         notes:notes
+//     })
+// });
+
+// app.delete("/notes/:index",(req,res) =>{
+
+//     const index = req.params.index
+
+//     delete notes [index]
+
+//     res.status(200).json({
+//         message:"note deleted successfully"
+//     })
+// });
+
+// app.patch("/notes/:index",(req,res) =>{
+
+//     const index = req.params.index
+//     const descreption = req.body.descreption
+
+//     notes[ index ].descreption = descreption
+
+//     res.status(200).json({
+//         message:"notes updated successfully"
+//     })
+
+// }); 
+
+app.post("/notes", async(req,res) =>{
+    
+    const data = req.body /* {tittle,descreption} */
+
+    await noteModel.create({
+        tittle:data.tittle,
+        descreption:data.descreption
     })
-});
 
-app.delete("/notes/:index",(req,res) =>{
-
-    const index = req.params.index
-
-    delete notes [index]
-
-    res.status(200).json({
-        message:"note deleted successfully"
+    res.status(201).jason({
+        message:"note created"
     })
-});
-
-app.patch("/notes/:index",(req,res) =>{
-
-    const index = req.params.index
-    const descreption = req.body.descreption
-
-    notes[ index ].descreption = descreption
-
-    res.status(200).json({
-        message:"notes updated successfully"
-    })
-
-}); 
+})
 
 module.exports=app;
