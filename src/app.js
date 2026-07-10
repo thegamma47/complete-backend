a463a7a1bd8a06edcfd0c155dde121b48215a53b
Abhay RAjpoot <thegamma551@gmail.com>
2026-07-10 06:04:15 +0530

    initial changes

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+
+const app = express();
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+
+    res.status(201).json({message:"notes created successfully"})
+});
+
+app.get("/notes",(req,res) =>{
+
+    res.status(200).json({
+        notes:notes
+    })
+});
+
+app.delete("/notes/:index",(req,res) =>{
+
+    const index = req.params.index
+
+    delete notes [index]
+
+    res.status(200).json({
+        message:"note deleted successfully"
+    })
+});
+
+app.patch("/notes/:index",(req,res) =>{
+
+    const index = req.params.index
+    const descreption = req.body.descreption
+
+    notes[ index ].descreption = descreption
+
+    res.status(200).json({
+        message:"notes updated successfully"
+    })
+
+});
+
+module.exports=app;
